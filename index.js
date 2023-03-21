b275c2a9f8ea1a48f6577f35b1321c0c8bd1acf1
NyakundiMauya <n.mauya.nm@gmail.com>
2023-03-21 17:45:47 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    let result = record.find((element, index, array) => {
+        return element.result === 'W'
+    })
+    return result ? result.year : result;
+}
